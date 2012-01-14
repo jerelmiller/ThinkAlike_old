d4df0860116f769904f69c525fcced2ef98be9e6
Jerel Miller <jermille@mines.edu>
2012-01-14 14:39:44 -0700

    Adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinkAlike::Application.config.secret_token = '1f2841001123fd787a4a188262b6499ca0456829ee0d2bae44842918e4536c401b6fa6ec6b8bcb58ca7ecbda5d480146e54babe3bf737ea3302976096ac80755'
